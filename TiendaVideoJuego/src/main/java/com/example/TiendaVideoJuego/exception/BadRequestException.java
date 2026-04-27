b2615c56da15e7238c6971426ee87b8f1b8a641e
Vicente <l.neira@duocuc.cl>
2026-04-27 19:32:16 -0400

    Delete TiendaVideoJuego directory
    carpeta innecesaria

--- TiendaVideoJuego/src/main/java/com/example/TiendaVideoJuego/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.TiendaVideoJuego.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String mensaje) {
-        super(mensaje);
-    }
-}
--- TiendaVideoJuego/src/main/java/com/example/TiendaVideoJuego/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.TiendaVideoJuego.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String mensaje) {
-        super(mensaje);
-    }
-}
