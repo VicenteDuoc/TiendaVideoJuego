da71db6a9ed8e4a4143bfe9077bf8de93ea3bcac
beyovanovich <be.yovanovich@duocuc.cl>
2026-04-27 09:45:09 -0400

    Add files via upload

---
+++ tiendavideojuego/Categoria/src/main/java/com/tiendavideojuego/Categoria/service/CategoriaService.java
@@ -0,0 +1,9 @@
+package com.tiendavideojuego.Categoria.service;
+
+import com.tiendavideojuego.Categoria.model.Categoria;
+import com.tiendavideojuego.Categoria.repository.CategoriaRepository;
+
+import java.util.List;
+
+public class CategoriaService {
+}
