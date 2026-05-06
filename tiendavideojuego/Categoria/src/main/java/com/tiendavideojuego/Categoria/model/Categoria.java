dce4ad1fc2a3bb435333e24a9936de15d71b12c0
beyovanovich <be.yovanovich@duocuc.cl>
2026-05-06 15:34:32 -0400

    Add files via upload

---
+++ tiendavideojuego/Categoria/src/main/java/com/tiendavideojuego/Categoria/model/Categoria.java
@@ -0,0 +1,44 @@
+package com.tiendavideojuego.Categoria.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categoria")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Categoria {
+
+    private Long id;
+    private String nombre;
+    private String descripcion;
+
+    public Categoria() {
+    }
+        public Categoria(Long id, String nombre, String descripcion) {
+            this.id = id;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public String getDescripcion() {
+            return descripcion;
+        }
+
+        public void setDescripcion(String descripcion) {
+            this.descripcion = descripcion;
+        }
+    }
---
+++ tiendavideojuego/producto/src/main/java/com/tiendavideojuego/producto/model/Producto.java
@@ -0,0 +1,29 @@
+package com.tiendavideojuego.producto.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "producto")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private double precio;
+
+    public Producto() {}
+
+    public Producto(Long id, String nombre, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    // getters y setters
+}
