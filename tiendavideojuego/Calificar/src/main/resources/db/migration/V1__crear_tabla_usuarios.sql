ea7edc4797c25eadae47adb16d606567ff3fb2c3
beyovanovich <be.yovanovich@duocuc.cl>
2026-05-07 16:35:36 -0400

    Delete tiendavideojuego directory

--- tiendavideojuego/Calificar/src/main/resources/db/migration/V1__crear_tabla_usuarios.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE calificacion (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    id_juego BIGINT,
-    puntuacion INT,
-    comentario VARCHAR(255)
-);
--- tiendavideojuego/Calificar/src/main/resources/db/migration/V2__datos_iniciales.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO calificacion (id_juego, puntuacion, comentario)
-VALUES
-(1, 10, 'Excelente juego'),
-(2, 8, 'Muy entretenido'),
-(3, 6, 'Regular');
