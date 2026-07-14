592363534e2671c6577e378d9a80b2ce640b13b2
beyovanovich <be.yovanovich@duocuc.cl>
2026-07-14 00:31:05 -0400

    Delete Carrito directory

--- Carrito/src/main/resources/db/migration/V1__crear_tabla_carrito.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE carrito_item (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario BIGINT NOT NULL,
-    id_producto BIGINT NOT NULL,
-    cantidad INT NOT NULL,
-    fecha_agregado DATETIME NOT NULL
-);
--- Carrito/src/main/resources/db/migration/V2__datos_iniciales.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO carrito_item (id_usuario, id_producto, cantidad, fecha_agregado) VALUES
-(1, 1, 1, NOW()),
-(1, 3, 2, NOW()),
-(2, 5, 1, NOW());
