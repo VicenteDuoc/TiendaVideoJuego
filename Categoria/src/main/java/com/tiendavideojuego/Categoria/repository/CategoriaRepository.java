665b8361e8192b06e04a7e11ebd4d454cbb68282
beyovanovich <be.yovanovich@duocuc.cl>
2026-07-14 00:31:27 -0400

    Delete Categoria directory

--- Categoria/src/main/java/com/tiendavideojuego/Categoria/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tiendavideojuego.Categoria.repository;
-
-import  com.tiendavideojuego.Categoria.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
