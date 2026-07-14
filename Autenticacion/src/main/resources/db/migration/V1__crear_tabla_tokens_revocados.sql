265e3a6edd18989e06c907951ec82fcf95809bf4
beyovanovich <be.yovanovich@duocuc.cl>
2026-07-14 00:30:52 -0400

    Delete Autenticacion directory

--- Autenticacion/src/main/resources/db/migration/V1__crear_tabla_tokens_revocados.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tokens_revocados (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    token VARCHAR(500) NOT NULL UNIQUE,
-    id_usuario BIGINT NOT NULL,
-    email VARCHAR(150) NOT NULL,
-    fecha_revocacion DATETIME NOT NULL
-);
